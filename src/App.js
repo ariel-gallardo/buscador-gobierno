add95d5b1008742df33a23c0b10383b6da30291b
Ariel Gallardo <ariel.gallardo96@gmail.com>
2022-07-05 23:14:32 -0300

    main# buscador-gobierno

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import Pages from './pages.json'
 
 function App() {
+
+  const [data, setData] = useState(Pages)
+  
+  const [escuela, setescuela] = useState('');
+  const [materia, setmateria] = useState('');
+  const [ciudad, setciudad] = useState('');
+  const [horas, sethoras] = useState('');
+  const [turno, setturno] = useState('');
+
+
+  const filterData = () => {
+   
+    setData(
+      Pages
+      .filter(e => e.Escuela.toString().toLocaleLowerCase().includes(escuela.toLocaleLowerCase()) )
+      .filter(e => e.Materia.toString().toLocaleLowerCase().includes(materia.toLocaleLowerCase()) )
+      .filter(e => e.Ciudad.toString().toLocaleLowerCase().includes(ciudad.toLocaleLowerCase()) )
+      .filter(e => e.Horas.toString().toLocaleLowerCase().includes(horas.toLocaleLowerCase()) )
+      .filter(e => e.Turno.toString().toLocaleLowerCase().includes(turno.toLocaleLowerCase()) )
+    )
+  }
+
+  useEffect(()=>{
+    if(escuela !== '' || materia!== '' || ciudad!== '' || horas !== '' || turno !== '' )
+      filterData()
+    else
+      setData(Pages)
+  },[escuela, materia, ciudad, horas, turno])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+      <input onChange={(e)=>{setciudad(e.target.value)}} value={ciudad} name="ciudad" placeholder="Departamento"/>
+        <input onChange={(e)=>{setescuela(e.target.value)}} value={escuela} name="escuela" placeholder="Escuela" />
+        <input onChange={(e)=>{setmateria(e.target.value)}} value={materia} name="materia" placeholder="Materia"/>
+        <input onChange={(e)=>{sethoras(e.target.value)}} value={horas} name="horas" placeholder="Horas"/>
+        <input onChange={(e)=>{setturno(e.target.value)}} value={turno} name="turno" placeholder="Turno"/>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope='col'>Departamento</th>
+            <th scope='col'>Escuela</th>
+            <th scope='col'>Materia</th>
+            <th scope='col'>Horas</th>
+            <th scope='col'>Turno</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((d,i) => <tr key={i} scope='row'>
+              <td className=''>{d.Ciudad.replace('0','').replace('5','').replace('3','').replace('7','')}</td>
+              <td className=''><b>{d.Escuela}</b></td>
+              <td className=''>{d.Materia}</td>
+              <td className=''><b>{d.Horas}Hs</b></td>
+              <td className=''><b>{d.Turno === 'T' ? 'Tarde' : 'Mañana'}</b></td>
+            </tr>)
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
